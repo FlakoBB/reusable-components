b2add638363655fc4c9a266db5d69cf6a1827e1f
FlakoBB <josephrbb99@gmail.com>
2023-08-09 16:06:41 -0600

    Challenge 1 completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,19 @@ import ButtonsSection from './components/ButtonsSection.jsx'
 
 const App = () => {
   return (
-    <div>
-      <h1>Reusable Components</h1>
+    <>
+      <header className='mainHeader'>
+        <h1 className='title'>Reusable Components</h1>
+        <nav className='navigator'>
+          <span className='navigator__link navigator__link--btns'>Buttons</span>
+          <span className='navigator__link navigator__link--inputs'>Inputs</span>
+        </nav>
+      </header>
       <ButtonsSection />
-    </div>
+      <footer>
+        <p>Created by <a href='https://bit.ly/follow-flako' target='_blank' rel='noreferrer'>FlakoBB</a> - devChallenches.io</p>
+      </footer>
+    </>
   )
 }
 export default App
--- src/components/ButtonsSection.jsx
+++ src/components/ButtonsSection.jsx
@@ -1,45 +1,92 @@
 import Button from './Button.jsx'
 
 const ButtonsSection = () => {
-  const sayHi = (name) => {
-    console.log(`Hola !!! ${name}`)
-  }
-
   return (
-    <section className='column'>
-      <h2>Buttons</h2>
-      <Button value='Click me!!' variant='default' />
-      <Button value='Click me!!' variant='default' disableShadow />
-      <Button value='Click me!!' color='primary' />
-      <Button value='Click me!!' color='primary' disableShadow />
-      <Button value='Click me!!' color='secondary' />
-      <Button value='Click me!!' color='secondary' disableShadow />
-      <Button value='Click me!!' color='danger' />
-      <Button value='Click me!!' color='danger' disableShadow />
-      <Button value='Click me!!' variant='outline' />
-      <Button value='Click me!!' variant='outline' color='primary' />
-      <Button value='Click me!!' variant='outline' color='secondary' />
-      <Button value='Click me!!' variant='outline' color='danger' />
-      <Button value='Click me!!' variant='text' />
-      <Button value='Click me!!' variant='text' color='primary' />
-      <Button value='Click me!!' variant='text' color='secondary' />
-      <Button value='Click me!!' variant='text' color='danger' />
-      <Button value='Hola' variant='default' size='sm' />
-      <Button value='Hola' variant='outline' size='md' />
-      <Button value='Hola' variant='outline' size='lg' color='danger' />
-      <Button value='Hola' variant='default' size='md' />
-      <Button value='Hola 1' variant='default' size='lg' disableShadow onClick={() => sayHi('BTN 1')} />
-      <Button value='Hola 2' variant='default' size='lg' disabled onClick={() => sayHi('BTN 2')} />
-      <Button />
-      <Button value='Custom' id='MyBTN' customClass='custom__btn' />
-      <Button value='Icon Start' variant='outline' color='danger' iconStart='sign-turn-left' />
-      <Button value='Icon End' variant='outline' color='danger' iconEnd='send-fill' />
-      <Button value='Botón con mucho texto para ver como se comporta el tamaño de un boton con mucho texto xD' size='sm' />
-      <Button value='Botón con mucho texto para ver como se comporta el tamaño de un boton con mucho texto xD' size='md' />
-      <Button value='Botón con mucho texto para ver como se comporta el tamaño de un boton con mucho texto xD' size='lg' />
-      <Button value='sm' size='sm' />
-      <Button value='md' size='md' />
-      <Button value='lg' size='lg' />
+    <section className='container'>
+      <h2 className='container__title'>Buttons</h2>
+      <main>
+        <article className='type_container'>
+          <h4 className='syntaxis'><code>&lt;Button /&gt;</code></h4>
+          <Button />
+        </article>
+        <article className='type_container'>
+          <h3 className='typeTitle'>Sizes</h3>
+          <div className='rowContent'>
+            <div>
+              <h4 className='syntaxis'><code>&lt;Button size='sm' /&gt;</code></h4>
+              <Button size='sm' />
+            </div>
+            <div>
+              <h4 className='syntaxis'><code>&lt;Button size='md' /&gt;</code></h4>
+              <Button size='md' />
+            </div>
+            <div>
+              <h4 className='syntaxis'><code>&lt;Button size='lg' /&gt;</code></h4>
+              <Button size='lg' />
+            </div>
+          </div>
+        </article>
+        <article className='type_container'>
+          <h3 className='typeTitle'>Colors</h3>
+          <div className='rowContent'>
+            <div>
+              <h4 className='syntaxis'><code>&lt;Button color='primary' /&gt;</code></h4>
+              <Button color='primary' />
+            </div>
+            <div>
+              <h4 className='syntaxis'><code>&lt;Button color='secondary' /&gt;</code></h4>
+              <Button color='secondary' />
+            </div>
+            <div>
+              <h4 className='syntaxis'><code>&lt;Button color='danger' /&gt;</code></h4>
+              <Button color='danger' />
+            </div>
+          </div>
+        </article>
+        <article className='type_container'>
+          <h3 className='typeTitle'>Variants</h3>
+          <div className='rowContent'>
+            <div>
+              <h4 className='syntaxis'><code>&lt;Button value='Click me!' variant='outline' /&gt;</code></h4>
+              <Button value='Click me!' variant='outline' />
+            </div>
+            <div>
+              <h4 className='syntaxis'><code>&lt;Button value='Click me!' variant='text' /&gt;</code></h4>
+              <Button value='Click me!' variant='text' />
+            </div>
+            <div>
+              <h4 className='syntaxis'><code>&lt;Button value='Click me!' variant='default' /&gt;</code></h4>
+              <Button value='Click me!' variant='default' />
+            </div>
+          </div>
+        </article>
+        <article className='type_container'>
+          <h4 className='syntaxis'><code>&lt;Button disableShadow /&gt;</code></h4>
+          <Button disableShadow />
+        </article>
+        <article className='type_container'>
+          <h4 className='syntaxis'><code>&lt;Button disabled /&gt;</code></h4>
+          <Button disabled />
+        </article>
+        <article className='type_container'>
+          <h3 className='typeTitle'>Icons</h3>
+          <div className='rowContent'>
+            <div>
+              <h4 className='syntaxis'><code>&lt;Button iconStart='send-fill' /&gt;</code></h4>
+              <Button iconStart='send-fill' />
+            </div>
+            <div>
+              <h4 className='syntaxis'><code>&lt;Button iconEnd='send-fill' /&gt;</code></h4>
+              <Button iconEnd='send-fill' />
+            </div>
+          </div>
+        </article>
+        <article className='type_container'>
+          <h3 className='typeTitle'>Custom Styles</h3>
+          <h4 className='syntaxis'><code>&lt;Button value='Custom' customClass='custom__btn' /&gt;</code></h4>
+          <Button value='Custom' customClass='custom__btn' />
+        </article>
+      </main>
     </section>
   )
 }
