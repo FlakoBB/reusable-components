f076c670df5dc48606e497b78e2b27276c8e04dc
FlakoBB <josephrbb99@gmail.com>
2023-08-12 16:22:44 -0600

    feat: colors

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,42 @@
 import PropTypes from 'prop-types'
 import styles from '../styles/inputs.module.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
-const Input = ({ id, label, placeholder, value, helperText, startIcon, endIcon, multiline, rows }) => {
+const COLORS = {
+  BLUE: 'blue',
+  GREEN: 'green',
+  PINK: 'pink',
+  PURPLE: 'purple'
+}
+
+const Input = ({ id, label, placeholder, value, helperText, startIcon, endIcon, color, multiline, rows }) => {
   const [isFocus, setIsFocus] = useState(false)
   const [inputValue, setInputValue] = useState(value)
+  const [inputColor, setInputColor] = useState(color)
+
+  const getColor = (clr) => {
+    switch (clr) {
+      case COLORS.BLUE:
+        setInputColor(styles.blue)
+        break
+      case COLORS.GREEN:
+        setInputColor(styles.green)
+        break
+      case COLORS.PINK:
+        setInputColor(styles.pink)
+        break
+      case COLORS.PURPLE:
+        setInputColor(styles.purple)
+        break
+      default:
+        setInputColor('')
+    }
+  }
+
+  useEffect(() => {
+    getColor(color)
+  }, [])
 
   const handleFocus = () => {
     setIsFocus(true)
@@ -23,7 +54,7 @@ const Input = ({ id, label, placeholder, value, helperText, startIcon, endIcon, 
   }
 
   return (
-    <div className={`${styles.inputGroup} ${isFocus && styles.focus}`}>
+    <div className={`${styles.inputGroup} ${isFocus && styles.focus} ${inputColor}`}>
       <label htmlFor={id} className={styles.label}>{label}</label>
       <div className={styles.fieldContainer}>
         {
@@ -68,6 +99,7 @@ Input.propTypes = {
   helperText: PropTypes.string,
   startIcon: PropTypes.string,
   endIcon: PropTypes.string,
+  color: PropTypes.string,
   multiline: PropTypes.bool,
   rows: PropTypes.string
 }
@@ -76,6 +108,7 @@ Input.defaultProps = {
   label: 'Label',
   placeholder: 'Placeholder',
   value: '',
+  color: '',
   rows: '4'
 }
 
--- src/components/InputsSection.jsx
+++ src/components/InputsSection.jsx
@@ -5,10 +5,10 @@ const InputsSection = () => {
     <div>
       <h2>Inputs</h2>
       <div className='temporalContainer'>
-        <Input id='gua' />
-        <Input id='dos' helperText='Helper Text' />
-        <Input id='tres' startIcon='person-fill' />
-        <Input id='cuatro' endIcon='person-fill' />
+        <Input id='gua' color='blue' />
+        <Input id='dos' helperText='Helper Text' color='green' />
+        <Input id='tres' startIcon='person-fill' color='pink' />
+        <Input id='cuatro' endIcon='person-fill' color='purple' />
         <Input id='cinco' endIcon='arrow-right' value='My own description' multiline rows='6' />
       </div>
     </div>
